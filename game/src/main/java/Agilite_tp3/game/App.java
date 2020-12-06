e8a3e2e66e264f682329e1021dae1113b5b1e2f9
Administrator <Administrator@DESKTOP-892TILQ>
2020-12-06 19:57:23 +0100

    this is our first version

---
+++ game/src/main/java/Agilite_tp3/game/App.java
@@ -0,0 +1,13 @@
+package Agilite_tp3.game;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
